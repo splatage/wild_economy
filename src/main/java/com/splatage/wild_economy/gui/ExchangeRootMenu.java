d6af6eaf63a29e287afc92a35fa8a7fde7245e31
Splatage.com <81358782+splatage@users.noreply.github.com>
2026-03-19 14:02:55 +1300

    Create ExchangeRootMenu.java

---
+++ src/main/java/com/splatage/wild_economy/gui/ExchangeRootMenu.java
@@ -0,0 +1,10 @@
+package com.splatage.wild_economy.gui;
+
+import org.bukkit.entity.Player;
+
+public final class ExchangeRootMenu {
+
+    public void open(final Player player) {
+        // Build and open root category menu
+    }
+}
