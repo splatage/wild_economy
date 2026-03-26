975add34831865bb7fb34b4610b40af492c23b22
Splatage.com <81358782+splatage@users.noreply.github.com>
2026-03-26 22:21:19 +1300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.clip:placeholderapi:2.11.5")
     compileOnly("org.xerial:sqlite-jdbc:3.46.1.3")
-    compileOnly("com.mysql:mysql-connector-j:9.0.0")
+    compileOnly("com.mysql:mysql-connector-j:9.6.0")
     compileOnly("com.zaxxer:HikariCP:5.1.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
@@ -32,7 +32,7 @@ dependencies {
     testImplementation("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
     testRuntimeOnly("org.xerial:sqlite-jdbc:3.46.1.3")
-    testRuntimeOnly("com.mysql:mysql-connector-j:9.0.0")
+    testRuntimeOnly("com.mysql:mysql-connector-j:9.6.0")
     testRuntimeOnly("com.zaxxer:HikariCP:5.1.0")
 }
 
