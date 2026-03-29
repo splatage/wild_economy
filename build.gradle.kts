dc28d58ddc222954377944f7440a756342637f78
splatage <noj.melville@gmail.com>
2026-03-30 00:30:09 +1300

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "com.splatage"
 
-val baseVersion = "0.1"
+val baseVersion = "0.2"
 val buildNumber = System.getenv("BUILD_NUMBER")
 val releaseBuild = System.getenv("RELEASE") == "true"
 
